c4128b6964274e082ebb7c6322d8d1c1ada39a12
José Manuel Cota <jmcota@ontica.org>
2024-11-24 09:10:18 -0600

    Empiria Orders and Requisitions Management modules added

---
+++ Orders/Tests/Empiria.Orders.Tests.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/*Empiria Operations *****************************************************************************************
+*                                                                                                            *
+*  System   : Orders and Requisitions Management           Module  : Test suite                              *
+*  Assembly : Empiria.Orders.Tests.dll                     Pattern : Assembly Attributes File                *
+*                                                          License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Tests module for Orders and Requisitions Management.                                           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
---
+++ Orders/WebApi/Empiria.Orders.WebApi.AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿/* Empiria Operations ****************************************************************************************
+*                                                                                                            *
+*  System   : Orders and Requisitions Management           Module  : Web Api Layer                           *
+*  Assembly : Empiria.Orders.WebApi.dll                    Pattern : Assembly Attributes File                *
+*                                                          License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : HTTP/JSON web api used to interact with the Empiria Orders and Requisitions                    *
+*             Management System's backend.                                                                   *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
