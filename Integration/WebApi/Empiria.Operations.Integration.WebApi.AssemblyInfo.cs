74b77533a4526e09429f6634c1ec6974ec58542a
José Manuel Cota <jmcota@ontica.org>
2024-11-26 07:26:49 -0600

    Operations Integration Web Api module added

---
+++ Integration/WebApi/Empiria.Operations.Integration.WebApi.AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿/* Empiria Integrated Operations Management ******************************************************************
+*                                                                                                            *
+*  System   : Integrated Operations Management             Module  : Web Api Layer                           *
+*  Assembly : Empiria.Operations.Integration.WebApi.dll    Pattern : Assembly Attributes File                *
+*                                                          License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : HTTP/JSON web api used to interact with the Empiria Integrated Operations                      *
+*             Management System's backend.                                                                   *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
