0325738ca11f63bc703ee3decc92e4a7e904e957
José Manuel Cota <jmcota@ontica.org>
2024-12-03 21:42:41 -0600

    PayableOrder and PayableOrderItem types added

---
+++ Orders/Core/Domain/PayableOrder.cs
@@ -0,0 +1,86 @@
+﻿/* Empiria Operations ****************************************************************************************
+*                                                                                                            *
+*  Module   : Orders Management                          Component : Domain Layer                            *
+*  Assembly : Empiria.Orders.Core.dll                    Pattern   : Information Holder                      *
+*  Type     : PayableOrder                               License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a payable order.                                                                    *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Collections.Generic;
+using Empiria.Financial;
+
+namespace Empiria.Orders {
+
+  /// <summary>Represents a payable order.</summary>
+  public class PayableOrder : Order, IPayableEntity {
+
+    #region Constructors and parsers
+
+    static public new PayableOrder Parse(int id) => ParseId<PayableOrder>(id);
+
+    static public new PayableOrder Parse(string uid) => ParseKey<PayableOrder>(uid);
+
+    static public new PayableOrder Empty => ParseEmpty<PayableOrder>();
+
+
+    #endregion Constructors and parsers
+
+    #region IPayableEntity interface
+
+    INamedEntity IPayableEntity.Type {
+      get {
+        return base.GetEmpiriaType();
+      }
+    }
+
+    string IPayableEntity.Name {
+      get {
+        return ((INamedEntity) this).Name;
+      }
+    }
+
+
+    string IPayableEntity.EntityNo {
+      get {
+        return base.OrderNo;
+      }
+    }
+
+    INamedEntity IPayableEntity.PayTo {
+      get {
+        return base.Beneficiary;
+      }
+    }
+
+    IEnumerable<IPayableEntityItem> IPayableEntity.Items {
+      get {
+        return base.GetItems<PayableOrderItem>()
+                   .ToFixedList();
+      }
+    }
+
+    #endregion IPayableEntity interface
+
+    #region Methods
+
+    internal protected virtual void AddItem(PayableOrderItem orderItem) {
+      Assertion.Require(orderItem, nameof(orderItem));
+
+      base.AddItem(orderItem);
+    }
+
+
+
+    internal protected virtual void RemoveItem(PayableOrderItem orderItem) {
+      Assertion.Require(orderItem, nameof(orderItem));
+
+      base.RemoveItem(orderItem);
+    }
+
+    #endregion Methods
+
+  }  // class PayableOrder
+
+}  // namespace Empiria.Orders
---
+++ Orders/Core/Domain/PayableOrderItem.cs
@@ -0,0 +1,90 @@
+﻿/* Empiria Operations ****************************************************************************************
+*                                                                                                            *
+*  Module   : Orders Management                          Component : Domain Layer                            *
+*  Assembly : Empiria.Orders.Core.dll                    Pattern   : Information Holder                      *
+*  Type     : PayableOrderItem                           License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a payable order item.                                                               *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Budgeting;
+using Empiria.Financial;
+
+namespace Empiria.Orders {
+
+  /// <summary>Represents a payable order item.</summary>
+  public class PayableOrderItem : OrderItem, IPayableEntityItem {
+
+    #region Constructors and parsers
+
+    static private PayableOrderItem Parse(int id) => ParseId<PayableOrderItem>(id);
+
+    static private PayableOrderItem Parse(string uid) => ParseKey<PayableOrderItem>(uid);
+
+    static internal new PayableOrderItem Empty => ParseEmpty<PayableOrderItem>();
+
+    #endregion Constructors and parsers
+
+    #region Propreties
+
+    [DataField("ORDER_ITEM_UNIT_PRICE")]
+    public decimal UnitPrice {
+      get; private set;
+    }
+
+
+    [DataField("ORDER_ITEM_CURRENCY_ID")]
+    public Currency Currency {
+      get; private set;
+    }
+
+
+    [DataField("ORDER_ITEM_BUDGET_ACCOUNT_ID")]
+    public BudgetAccount BudgetAccount {
+      get; private set;
+    }
+
+
+    public decimal Total {
+      get {
+        return Quantity * UnitPrice;
+      }
+    }
+
+    #endregion Properties
+
+    #region IPayableEntityItem implementation
+
+    INamedEntity IPayableEntityItem.BudgetAccount {
+      get {
+        return BudgetAccount.MapToNamedEntity();
+      }
+    }
+
+
+    INamedEntity IPayableEntityItem.Currency {
+      get {
+        return Currency.MapToNamedEntity();
+      }
+    }
+
+
+    INamedEntity IPayableEntityItem.Product {
+      get {
+        return base.Product.MapToNamedEntity();
+      }
+    }
+
+
+    INamedEntity IPayableEntityItem.Unit {
+      get {
+        return base.QuantityUnit.MapToNamedEntity();
+      }
+    }
+
+    #endregion IPayableEntityItem implementation
+
+  }  // class PayableOrderItem
+
+}  // namespace Empiria.Orders
