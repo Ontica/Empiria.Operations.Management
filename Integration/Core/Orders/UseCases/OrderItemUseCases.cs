98caeb903eda4c28b858f4ac81b91a6cc8a7e4b2
José Manuel Cota <jmcota@ontica.org>
2024-12-23 22:36:20 -0600

    OrderItem web api and use cases added (for contract items only)

---
+++ Integration/Core/Orders/UseCases/OrderItemUseCases.cs
@@ -0,0 +1,91 @@
+﻿/* Empiria Integrated Operations Management ******************************************************************
+*                                                                                                            *
+*  Module   : Orders Management Integration                 Component : Use cases Layer                      *
+*  Assembly : Empiria.Operations.Integration.Core.dll       Pattern   : Use case interactor class            *
+*  Type     : ContractOrderItemUseCases                     License   : Please read LICENSE.txt file         *
+*                                                                                                            *
+*  Summary  : Use cases used to create and update order item information.                                    *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Services;
+
+using Empiria.Procurement.Contracts;
+using Empiria.Procurement.Contracts.Adapters;
+
+namespace Empiria.Operations.Integration.Orders.UseCases {
+
+  /// <summary>Use cases used to create and update order item information.</summary>
+  public class ContractOrderItemUseCases : UseCase {
+
+    #region Constructors and parsers
+
+    protected ContractOrderItemUseCases() {
+      // no-op
+    }
+
+    static public ContractOrderItemUseCases UseCaseInteractor() {
+      return CreateInstance<ContractOrderItemUseCases>();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Use cases
+
+    public ContractOrderItemDto CreateOrderItem(string orderUID, ContractOrderItemFields fields) {
+      Assertion.Require(orderUID, nameof(orderUID));
+      Assertion.Require(fields, nameof(fields));
+
+      var contractOrder = ContractOrder.Parse(orderUID);
+      var contractItem = ContractItem.Parse(fields.ContractItemUID);
+
+      var item = new ContractOrderItem(contractOrder, contractItem);
+
+      item.Update(fields);
+
+      contractOrder.AddItem(item);
+
+      item.Save();
+
+      return ContractOrderMapper.Map(item);
+    }
+
+
+    public ContractOrderItemDto DeleteOrderItem(string orderUID, string orderItemUID) {
+      Assertion.Require(orderUID, nameof(orderUID));
+      Assertion.Require(orderItemUID, nameof(orderItemUID));
+
+      var order = ContractOrder.Parse(orderUID);
+
+      var item = order.GetItem<ContractOrderItem>(orderItemUID);
+
+      order.RemoveItem(item);
+
+      item.Save();
+
+      return ContractOrderMapper.Map(item);
+    }
+
+
+    public ContractOrderItemDto UpdateOrderItem(string orderUID,
+                                                string orderItemUID,
+                                                ContractOrderItemFields fields) {
+      Assertion.Require(orderUID, nameof(orderUID));
+      Assertion.Require(fields, nameof(fields));
+
+      var order = ContractOrder.Parse(orderUID);
+
+      var item = order.GetItem<ContractOrderItem>(orderItemUID);
+
+      item.Update(fields);
+
+      item.Save();
+
+      return ContractOrderMapper.Map(item);
+    }
+
+    #endregion Use cases
+
+  }  // class OrderItemUseCases
+
+}  // namespace Empiria.Operations.Integration.Orders.UseCases
---
+++ Integration/WebApi/Orders/OrderItemController.cs
@@ -0,0 +1,78 @@
+﻿/* Empiria Integrated Operations Management ******************************************************************
+*                                                                                                            *
+*  Module   : Orders Management Integration                 Component : Web Api Layer                        *
+*  Assembly : Empiria.Operations.Integration.WebApi.dll     Pattern   : Web Api Controller                   *
+*  Type     : OrderController                               License   : Please read LICENSE.txt file         *
+*                                                                                                            *
+*  Summary  : Web api used to handle integrated orders items edition.                                        *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Web.Http;
+
+using Empiria.WebApi;
+
+using Empiria.Orders.Adapters;
+
+using Empiria.Procurement.Contracts;
+using Empiria.Procurement.Contracts.Adapters;
+
+using Empiria.Operations.Integration.Orders.UseCases;
+
+
+namespace Empiria.Operations.Integration.Orders.WebApi {
+
+  /// <summary>Web api used to handle integrated orders items edition.</summary>
+  public class OrderItemController : WebApiController {
+
+    #region Web Apis
+
+
+    [HttpPost]
+    [Route("v8/order-management/orders/{orderUID:guid}/items")]
+    public SingleObjectModel CreateOrderItem([FromUri] string orderUID,
+                                             [FromBody] ContractOrderItemFields fields) {
+
+      base.RequireBody(fields);
+
+      using (var usecases = ContractOrderItemUseCases.UseCaseInteractor()) {
+        ContractOrderItemDto orderItem = usecases.CreateOrderItem(orderUID, fields);
+
+        return new SingleObjectModel(base.Request, orderItem);
+      }
+    }
+
+
+    [HttpDelete]
+    [Route("v8/order-management/orders/{orderUID:guid}/items/{orderItemUID:guid}")]
+    public NoDataModel RemoveOrderItem([FromUri] string orderUID,
+                                       [FromUri] string orderItemUID) {
+
+      using (var usecases = ContractOrderItemUseCases.UseCaseInteractor()) {
+        _ = usecases.DeleteOrderItem(orderUID, orderItemUID);
+
+        return new NoDataModel(base.Request);
+      }
+    }
+
+
+    [HttpPut, HttpPatch]
+    [Route("v8/order-management/orders/{orderUID:guid}/items/{orderItemUID:guid}")]
+    public SingleObjectModel UpdateOrderItem([FromUri] string orderUID,
+                                             [FromUri] string orderItemUID,
+                                             [FromBody] ContractOrderItemFields fields) {
+
+      base.RequireBody(fields);
+
+      using (var usecases = ContractOrderItemUseCases.UseCaseInteractor()) {
+        ContractOrderItemDto orderItem = usecases.UpdateOrderItem(orderUID, orderItemUID, fields);
+
+        return new SingleObjectModel(base.Request, orderItem);
+      }
+    }
+
+    #endregion Web Apis
+
+  }  // class OrderItemController
+
+}  // namespace Empiria.Operations.Orders.WebApi
