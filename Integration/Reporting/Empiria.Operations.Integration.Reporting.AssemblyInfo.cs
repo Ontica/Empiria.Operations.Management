cfd9441b794442a962b127535a8fe74d729034d0
José Manuel Cota <jmcota@ontica.org>
2025-02-19 22:03:49 -0500

    Integrated operations management reporting module added

---
+++ Integration/Reporting/Empiria.Operations.Integration.Reporting.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Integrated Operations Management ******************************************************************
+*                                                                                                            *
+*  System   : Integrated Operations Management               Module  : Reporting                             *
+*  Assembly : Empiria.Operations.Integration.Reporting.dll   Pattern : Assembly Attributes File              *
+*                                                            License : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Provides services to obtain integrated operations management information and reports.          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
