fc7e81d043306534aa8bcd4d8389bdae2e640b17
efrain-jerv <efrain.rodriguez@ontica.org>
2025-04-10 21:01:28 -0700

    Methods to insert or update inventory orders and inventory entries added in data layer

--- Inventory/Core/Materials/Data/InventoryOrderData.cs
+++ Inventory/Core/Materials/Data/InventoryOrderData.cs
@@ -15,17 +15,35 @@ namespace Empiria.Inventory.Data {
 
   /// <summary>Provides data read and write methods for inventory order instances.</summary>
   static internal class InventoryOrderData {
+    
+    internal static void WriteInventoryEntry(InventoryEntry entry) {
+
+      var op = DataOperation.Parse("write_OMS_Inventory_Entry",
+          entry.Id, entry.UID,
+          entry.InventoryEntryTypeId, entry.InventoryOrder.Id, 
+          entry.ProductId, entry.SkuId,
+          entry.LocationId, entry.ObservationNotes,
+          entry.UnitId, entry.InputQuantity,
+          entry.InputCost, entry.OutputQuantity,
+          entry.OutputCost, entry.EntryTime,
+          entry.Tags, entry.ExtData,
+          entry.InventoryEntryKeywords, entry.PostedById,
+          entry.PostingTime, (char) entry.Status);
+
+      DataWriter.Execute(op);
+    }
+
 
     internal static void WriteInventoryOrder(InventoryOrder order) {
       
-      var op = DataOperation.Parse("writeInventoryOrder",
-          order.InventoryOrderId, order.InventoryOrderUID,
+      var op = DataOperation.Parse("write_OMS_Inventory_Order",
+          order.Id, order.UID,
           order.InventoryOrderTypeId, order.InventoryOrderNo,
-          order.Reference.Id, order.Responsible.Id,
-          order.AssignedTo.Id, order.Notes,
+          order.Reference, order.Responsible,
+          order.AssignedTo, order.Notes,
           order.InventoryOrderExtData, order.Keywords,
           order.ScheduledTime, order.ClosingTime,
-          order.PostingTime, order.PostedBy.Id,
+          order.PostingTime, order.PostedBy,
           (char) order.Status);
 
       DataWriter.Execute(op);
