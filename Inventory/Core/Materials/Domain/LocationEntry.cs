1c59a5d25e593f28961c1d9abfc28b01baca9380
efrain-jerv <efrain.rodriguez@ontica.org>
2025-04-18 20:11:07 -0700

    Objects to get specific data for product and location added on inventory

---
+++ Inventory/Core/Materials/Domain/LocationEntry.cs
@@ -0,0 +1,49 @@
+﻿/* Empiria Operations ****************************************************************************************
+*                                                                                                            *
+*  Module   : Material Management                        Component : Domain Layer                            *
+*  Assembly : Empiria.Inventory.Core.dll                 Pattern   : Partitioned Type / Information Holder   *
+*  Type     : LocationEntry                              License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a location entry.                                                                   *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System;
+
+namespace Empiria.Inventory {
+
+  /// <summary>Represents a location entry.</summary>
+  public class LocationEntry {
+
+    [DataField("Object_Id")]
+    internal int LocationId {
+      get; set;
+    }
+
+
+    [DataField("Object_UID")]
+    internal string LocationUID {
+      get; set;
+    }
+
+
+    [DataField("Object_Type_Id")]
+    internal int ObjectTypeId {
+      get; set;
+    }
+
+
+    [DataField("Object_Name")]
+    internal string Name {
+      get; set;
+    }
+
+
+    [DataField("Object_Description")]
+    internal string Description {
+      get; set;
+    }
+
+  } // class LocationEntry
+
+} // namespace Empiria.Inventory
---
+++ Inventory/Core/Materials/Domain/ProductEntry.cs
@@ -0,0 +1,49 @@
+﻿/* Empiria Operations ****************************************************************************************
+*                                                                                                            *
+*  Module   : Material Management                        Component : Domain Layer                            *
+*  Assembly : Empiria.Inventory.Core.dll                 Pattern   : Partitioned Type / Information Holder   *
+*  Type     : ProductEntry                               License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a product entry.                                                                    *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System;
+
+namespace Empiria.Inventory {
+
+  /// <summary>Represents a product entry.</summary>
+  public class ProductEntry {
+
+    [DataField("Product_Id")]
+    internal int ProductId {
+      get; set;
+    }
+
+
+    [DataField("Product_UID")]
+    internal string ProductUID {
+      get; set;
+    }
+
+
+    [DataField("Product_Type_Id")]
+    internal int ProductTypeId {
+      get; set;
+    }
+
+
+    [DataField("Product_Name")]
+    internal string Name {
+      get; set;
+    }
+
+
+    [DataField("Product_Description")]
+    internal string ProductDescription {
+      get; set;
+    }
+
+  } // class ProductEntry
+
+} // namespace Empiria.Inventory
