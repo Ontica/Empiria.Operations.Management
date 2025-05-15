9f70ee31ca74ba0014090eed74237cdf15f6b20b
hugoageont <hugo.garcia@ontica.org>
2025-05-15 14:26:31 -0600

    add Sales Order Items and Test

---
+++ Orders/Core/Domain/SalesOrderItem.cs
@@ -0,0 +1,47 @@
+﻿/* Empiria Operations ****************************************************************************************
+*                                                                                                            *
+*  Module   : Orders Management                          Component : Domain Layer                            *
+*  Assembly : Empiria.Orders.Core.dll                    Pattern   : Information Holder                      *
+*  Type     : SalesOrderItem                             License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents an abstract sales order item.                                                             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+
+using Empiria.Orders.Data;
+namespace Empiria.Orders {
+
+
+  /// <summary>Represents an abstract sales order item.</summary>
+  internal class SalesOrderItem : OrderItem {
+
+      #region Constructors and parsers
+    
+      protected SalesOrderItem(OrderItemType powertype) : base(powertype) {
+        // Required by Empiria Framework for all partitioned types.
+      }
+
+      internal SalesOrderItem() {
+        //no-op
+      }
+
+      static public new SalesOrderItem Parse(int id) => ParseId<SalesOrderItem>(id);
+
+      static public new SalesOrderItem Parse(string uid) => ParseKey<SalesOrderItem>(uid);
+
+      static public new SalesOrderItem Empty => ParseEmpty<SalesOrderItem>();
+
+      #endregion Constructors and parsers
+
+      #region Properties
+    
+      internal FixedList<Order> Orders {
+        get; set;
+      }
+    
+      #endregion Properties
+
+
+    }// class SalesOrderItem
+  }// namespace Empiria.Orders.Domain 
--- Orders/Tests/Core/OrderItemTests.cs
+++ Orders/Tests/Core/OrderItemTests.cs
@@ -20,6 +20,23 @@ namespace Empiria.Tests.Orders {
     #region Facts
 
     [Fact]
+    public void GetSalesOrderItemTest() {
+
+      SalesOrderItem sut = SalesOrderItem.Parse(1903);
+
+      Assert.NotNull(sut);
+    }
+
+
+    [Fact]
+    public void Should_Read_All_Sales_Orders_Items() {
+ 
+      var salesOrderItems = SalesOrderItem.Parse(1902);
+
+      Assert.NotNull(salesOrderItems);
+    }
+
+    [Fact]
     public void Should_Read_All_Orders_Items() {
       var orderItems = BaseObject.GetFullList<OrderItem>();
 
