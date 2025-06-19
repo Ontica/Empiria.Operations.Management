8007d00d0c49fa5d786e677c99dc6416568a3e9b
cyanez <cyanez@ontica.org>
2025-06-19 10:06:22 -0600

    Order inventory test added.

---
+++ Inventory/Tests/Materials/InventoryTests.cs
@@ -0,0 +1,60 @@
+﻿/* Empiria Operations ****************************************************************************************
+*                                                                                                            *
+*  Module   : Material Management                        Component : Domain Layer                            *
+*  Assembly : Empiria.Inventory.Core.dll                 Pattern   : Partitioned Type / Information Holder   *
+*  Type     : InventoryTests                             License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Unit tests for Inventory.                                                                      *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Inventory;
+
+using Xunit;
+
+namespace Empiria.Tests.Inventory {
+
+  /// <summary>Unit tests for Inventory.</summary>
+  public class InventoryTests {
+
+    #region Initialization
+
+    public InventoryTests() {
+      //TestsCommonMethods.Authenticate();
+    }
+
+    #endregion Initialization
+
+    [Fact]
+    public void Should_Parse_All_Inventory_orders() {
+      var orders = BaseObject.GetList<InventoryOrder>();
+
+      foreach (var sut in orders) {
+        Assert.NotEmpty(sut.OrderNo);
+        Assert.NotEmpty(sut.Description);        
+      }
+    }
+
+
+    [Fact]
+    public void Should_Read_All_Inventory_orders() {
+      var sut = BaseObject.GetList<InventoryOrder>();
+
+      Assert.NotNull(sut);
+      Assert.NotEmpty(sut);
+    }
+
+
+    [Fact]
+    public void Should_Read_Empty_InventoryOrder() {
+      var sut = InventoryOrder.Empty;
+
+      Assert.NotNull(sut);
+      Assert.Equal(InventoryOrder.Parse("Empty"), sut);
+      Assert.Equal(-1, sut.Id);
+    }
+
+
+  } // class InventoryTests
+
+} // namespace Empiria.Tests.Inventory
