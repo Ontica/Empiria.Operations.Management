a4d9a4a52577b0e137d36a5beffc9d63c37322e6
José Manuel Cota <jmcota@ontica.org>
2025-06-29 10:42:37 -0400

    Search assets assignations web api and use case added

---
+++ Inventory/Core/Assets/Domain/AssetAssignation.cs
@@ -0,0 +1,70 @@
+﻿/* Empiria Operations ****************************************************************************************
+*                                                                                                            *
+*  Module   : Assets Management                          Component : Domain Layer                            *
+*  Assembly : Empiria.Inventory.Core.dll                 Pattern   : Information Holder                      *
+*  Type     : AssetAssignation                           License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents an asset assignation.                                                               *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Locations;
+using Empiria.Parties;
+
+namespace Empiria.Inventory.Assets {
+
+  /// <summary>Represents an asset assignation.</summary>
+  public class AssetAssignation {
+
+    #region Properties
+
+    public string UID {
+      get {
+        return $"{AssignedTo.Id}|{AssignedToOrgUnit.Id}|{Location.Id}";
+      }
+    }
+
+
+    [DataField("ASSET_ASSIGNED_TO_ID")]
+    public Person AssignedTo {
+      get; private set;
+    }
+
+
+    [DataField("ASSET_ASSIGNED_TO_ORG_UNIT_ID")]
+    public OrganizationalUnit AssignedToOrgUnit {
+      get; private set;
+    }
+
+
+    [DataField("ASSET_LOCATION_ID")]
+    public Location Location {
+      get; private set;
+    }
+
+
+    public Location Building {
+      get {
+        return Location.SeekTree(LocationType.Building);
+      }
+    }
+
+
+    public Location Floor {
+      get {
+        return Location.SeekTree(LocationType.Floor);
+      }
+    }
+
+
+    public Location Place {
+      get {
+        return Location.SeekTree(LocationType.Place);
+      }
+    }
+
+    #endregion Properties
+
+  }  // class AssetAssignation
+
+}  // namespace Empiria.Inventory.Assets
---
+++ Inventory/Core/Assets/UseCases/AssetAssignationUseCases.cs
@@ -0,0 +1,52 @@
+﻿/* Empiria Operations ****************************************************************************************
+*                                                                                                            *
+*  Module   : Assets Management                          Component : Use cases Layer                         *
+*  Assembly : Empiria.Inventory.Core.dll                 Pattern   : Use case interactor class               *
+*  Type     : AssetAssignationUseCases                   License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Use cases for assets assignations.                                                             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Services;
+
+using Empiria.Inventory.Assets.Adapters;
+using Empiria.Inventory.Assets.Data;
+
+namespace Empiria.Inventory.Assets.UseCases {
+
+  /// <summary>Use cases for assets assignations.</summary>
+  public class AssetAssignationUseCases : UseCase {
+
+    #region Constructors and parsers
+
+    protected AssetAssignationUseCases() {
+      // no-op
+    }
+
+    static public AssetAssignationUseCases UseCaseInteractor() {
+      return UseCase.CreateInstance<AssetAssignationUseCases>();
+    }
+
+
+    #endregion Constructors and parsers
+
+    #region Use cases
+
+    public FixedList<AssetAssignationDescriptor> SearchAssetAssignations(AssetsAssignationsQuery query) {
+      Assertion.Require(query, nameof(query));
+
+      string filter = query.MapToFilterString();
+
+      string sort = query.MapToSortString();
+
+      FixedList<AssetAssignation> assignations = AssetsAssignationsData.SearchAssignations(filter, sort);
+
+      return AssetAssignationMapper.Map(assignations);
+    }
+
+    #endregion Use cases
+
+  }  // class AssetAssignationUseCases
+
+}  // namespace Empiria.Inventory.Assets.UseCases
