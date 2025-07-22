37c30734522e9f92db94fcb49cd5d257230a5995
hugoageont <hugo.garcia@ontica.org>
2025-07-22 16:36:47 -0600

    Create Sales Order and tests

---
+++ Orders/Core/Data/SalesOrdersData.cs
@@ -0,0 +1,49 @@
+﻿/* Empiria Operations ****************************************************************************************
+*                                                                                                            *
+*  Module   : Orders Management                          Component : Data Layer                              *
+*  Assembly : Empiria.Orders.Core.dll                    Pattern   : Data service                            *
+*  Type     : SalesOrdersData                            License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides data read and write methods for Salesorder and Sales order item instances.            *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Data;
+
+namespace Empiria.Orders.Data {
+
+  /// <summary>Provides data read and write methods for order and order item instances.</summary>
+  static internal class SalesOrdersData {
+
+    #region Methods
+
+    static internal void WriteSalesOrder(SalesOrder o, string extensionData) {
+      var op = DataOperation.Parse("write_OMS_Order",
+                     o.Id, o.UID, o.OrderType.Id, o.Category.Id, o.OrderNo, o.Description,
+                     EmpiriaString.Tagging(o.Identificators), EmpiriaString.Tagging(o.Tags),
+                     o.RequestedBy.Id, o.Responsible.Id, o.Beneficiary.Id, o.Provider.Id,
+                     -1, -1, -1, o.Project.Id, o.Currency.Id,
+                     o.Source.Id, (char) o.Priority, o.AuthorizationTime, o.AuthorizedBy.Id,
+                     o.ClosingTime, o.ClosedBy.Id, extensionData, o.Keywords, -1,
+                     o.PostedBy.Id, o.PostingTime, (char) o.Status);
+
+      DataWriter.Execute(op);
+    }
+
+
+    static internal void WriteSalesOrderItem(SalesOrderItem o, string extensionData) {
+      var op = DataOperation.Parse("write_OMS_Order_Item",
+                     o.Id, o.UID, o.OrderItemType.Id, o.Order.Id, o.Product.Id,
+                     o.Description, o.ProductUnit.Id, o.Quantity, o.UnitPrice, o.Discount,
+                     o.Currency.Id, o.RelatedItemId, o.RequisitionItemId, o.RequestedBy.Id,
+                     o.BudgetAccount.Id, o.Project.Id, o.Provider.Id, o.PerEachItemId, extensionData,
+                     o.Keywords, -1, o.Position, o.PostedBy.Id, o.PostingTime, (char) o.Status);
+
+      DataWriter.Execute(op);
+    }
+
+    #endregion Methods
+
+  }  // class SalesOrdersData
+
+}  // namespace Empiria.Orders.Data
---
+++ Orders/Core/Domain/SalesOrderType.cs
@@ -0,0 +1,57 @@
+﻿/* Empiria Financial  ****************************************************************************************
+*                                                                                                            *
+*  Module   : Material Management                        Component : Domain Layer                            *
+*  Assembly : Empiria.Inventory.Core.dll                 Pattern   : Common Storage Type                     *
+*  Type     : InventoryType                              License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Describes inventory type.                                                               *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System;
+using Empiria.Orders;
+
+namespace Empiria.Orders {
+
+  /// <summary>Describes inventory type.</summary>
+  public class SalesOrderType : OrderCategory {
+
+    #region Constructors and parsers
+
+    static public new SalesOrderType Parse(int id) => ParseId<SalesOrderType>(id);
+
+    static public new SalesOrderType Parse(string uid) => ParseKey<SalesOrderType>(uid);
+
+    static public new SalesOrderType Empty => ParseEmpty<SalesOrderType>();
+
+    static public new FixedList<SalesOrderType> GetList() {
+      return GetStorageObjects<SalesOrderType>();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public Boolean EntriesRequired {
+      get {
+        return base.ExtData.Get("entriesRequired", false);
+      }
+      private set {
+        base.ExtData.SetIfValue("entriesRequired", value);
+      }
+    }
+
+    public Boolean ItemsRequired {
+      get {
+        return base.ExtData.Get("itemsRequired", false);
+      }
+      private set {
+        base.ExtData.SetIfValue("itemsRequired", value);
+      }
+    }
+
+    #endregion Properties
+
+  } // class SalesOrderType
+
+} // namespace Empiria.Orders
