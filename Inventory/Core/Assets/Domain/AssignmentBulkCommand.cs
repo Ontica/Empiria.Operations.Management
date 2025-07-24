ee0c47b6b6d8fc91c71c2f3502c5645a245442cf
José Manuel Cota <jmcota@ontica.org>
2025-07-24 12:16:31 -0400

    AssetAssignmentUseCases.CreateBulkTransaction() methods completed

--- Inventory/Core/Assets/Domain/AssignmentBulkCommand.cs
+++ Inventory/Core/Assets/Domain/AssignmentBulkCommand.cs
@@ -8,13 +8,113 @@
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
+using System;
+using System.Collections.Generic;
+
 namespace Empiria.Inventory.Assets {
 
   /// <summary>Command data structure used to create assets transactions from assets assignments.</summary>
   public class AssignmentBulkCommand {
 
-    public string OperationID {
+    public AssetTransactionType TransactionType {
+      get; internal set;
+    } = AssetTransactionType.Empty;
+
+
+    public string[] Assignments {
       get; set;
+    } = new string[0];
+
+
+    public string[] Assets {
+      get; set;
+    } = new string[0];
+
+
+    public DateTime ApplicationDate {
+      get; set;
+    } = DateTime.Today;
+
+
+    public string AssignedToUID {
+      get; set;
+    } = string.Empty;
+
+
+    public string AssignedToOrgUnitUID {
+      get; set;
+    } = string.Empty;
+
+
+    public string LocationUID {
+      get; set;
+    } = string.Empty;
+
+
+    public string Description {
+      get; set;
+    } = string.Empty;
+
+
+    public string[] Identificators {
+      get; set;
+    } = new string[0];
+
+
+    public string[] Tags {
+      get; set;
+    } = new string[0];
+
+
+    internal FixedList<AssetTransactionEntryFields> GetTransactionEntryFields(AssetAssignment assignment) {
+      int size = Math.Max(Assets.Length, assignment.GetAssets().Count);
+
+      var list = new List<AssetTransactionEntryFields>(size);
+
+      if (Assets.Length > 0) {
+        foreach (var assetEntryUID in Assets) {
+          var txnEntry = AssetTransactionEntry.Parse(assetEntryUID);
+
+          AssetTransactionEntryFields entryFields = GetTransactionEntryFields(txnEntry);
+
+          list.Add(entryFields);
+        }
+        return list.ToFixedList();
+      }
+
+      foreach (var txnEntry in assignment.Transaction.Entries) {
+        AssetTransactionEntryFields entryFields = GetTransactionEntryFields(txnEntry);
+
+        list.Add(entryFields);
+      }
+
+      return list.ToFixedList();
+    }
+
+
+    internal AssetTransactionFields GetTransactionFields(AssetAssignment assignment) {
+      return new AssetTransactionFields {
+        ApplicationDate = ApplicationDate,
+        AssignedToUID = AssignedToUID,
+        AssignedToOrgUnitUID = AssignedToOrgUnitUID,
+        ReleasedByUID = assignment.AssignedTo.UID,
+        ReleasedByOrgUnitUID = assignment.AssignedToOrgUnit.UID,
+        Description = Description,
+        Identificators = Identificators,
+        Tags = Tags,
+        LocationUID = LocationUID,
+        TransactionTypeUID = TransactionType.UID
+      };
+    }
+
+
+    private AssetTransactionEntryFields GetTransactionEntryFields(AssetTransactionEntry entry) {
+      return new AssetTransactionEntryFields {
+         AssetUID = entry.Asset.UID,
+         EntryTypeUID = TransactionType.DefaultAssetTransactionEntryType.UID,
+         PreviousCondition = entry.Asset.CurrentCondition,
+         ReleasedCondition = entry.Asset.CurrentCondition
+      };
     }
 
   }  // class AssignmentBulkCommand
--- Inventory/Core/Assets/UseCases/AssetAssignmentUseCases.cs
+++ Inventory/Core/Assets/UseCases/AssetAssignmentUseCases.cs
@@ -8,6 +8,8 @@
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
+using System.Collections.Generic;
+
 using Empiria.Services;
 
 using Empiria.Inventory.Assets.Adapters;
@@ -33,10 +35,44 @@ namespace Empiria.Inventory.Assets.UseCases {
 
     #region Use cases
 
-    public AssetTransaction CreateBulkTransaction(AssignmentBulkCommand command) {
-      return AssetTransaction.Parse(1);
+
+    public AssetTransaction CreateBulkTransaction(string assignmentUID, AssignmentBulkCommand command) {
+      Assertion.Require(assignmentUID, nameof(assignmentUID));
+      Assertion.Require(command, nameof(command));
+
+      var assignment = AssetAssignment.Parse(assignmentUID);
+
+      var txn = new AssetTransaction(command.TransactionType);
+
+      AssetTransactionFields fields = command.GetTransactionFields(assignment);
+
+      txn.Update(fields);
+
+      FixedList<AssetTransactionEntryFields> cmdEntryFields = command.GetTransactionEntryFields(assignment);
+
+      foreach (var entryFields in cmdEntryFields) {
+        txn.AppendEntry(entryFields);
+      }
+
+      txn.Save();
+
+      return txn;
     }
 
+
+    public FixedList<AssetTransaction> CreateBulkTransactions(AssignmentBulkCommand command) {
+      Assertion.Require(command, nameof(command));
+
+      List<AssetTransaction> txns = new List<AssetTransaction>(command.Assignments.Length);
+
+      foreach (var assignmentUID in command.Assignments) {
+        AssetTransaction txn = CreateBulkTransaction(assignmentUID, command);
+
+        txns.Add(txn);
+      }
+
+      return txns.ToFixedList();
+    }
 
     public AssetAssignmentHolder GetAssetAssignment(string assignmentUID) {
       Assertion.Require(assignmentUID, nameof(assignmentUID));
