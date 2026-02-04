be4c7aea69b7403d4df5b456c7f7264b4bbf1b9c
José Manuel Cota <jmcota@ontica.org>
2026-02-03 22:23:51 -0500

    Fix OrderItemFields.Quantity := Subtotal for MoneyBased product units

--- Orders/Core/Domain/OrderItemFields.cs
+++ Orders/Core/Domain/OrderItemFields.cs
@@ -174,23 +174,18 @@ namespace Empiria.Orders {
         UnitPrice = 1;
       }
 
+      Assertion.Require(UnitPrice > 0, "El precio unitario debe ser mayor a cero.");
+      Assertion.Require(Discount >= 0, "El descuento no puede ser negativo.");
+      Assertion.Require(PenaltyDiscount >= 0, "El descuento por penalización no puede ser negativo.");
+      Assertion.Require(Quantity > 0, "Necesito se proporcione la cantidad.");
+
       Assertion.Require(ProductUnitUID, "Se requiere proporcionar la unidad de medida.");
 
       var productUnit = ProductUnit.Parse(ProductUnitUID);
 
       if (productUnit.MoneyBased) {
         Assertion.Require(UnitPrice == 1m, "El precio unitario debe ser igual a uno.");
-        Quantity = Subtotal;
-
-      } else {
-        Assertion.Require(UnitPrice > 0, "El precio unitario debe ser mayor a cero.");
       }
-
-      Assertion.Require(Quantity > 0, "Necesito se proporcione la cantidad.");
-
-      Assertion.Require(UnitPrice > 0, "El precio unitario debe ser mayor a cero.");
-      Assertion.Require(Discount >= 0, "El descuento no puede ser negativo.");
-      Assertion.Require(PenaltyDiscount >= 0, "El descuento por penalización no puede ser negativo.");
 
       if (!StartDate.HasValue) {
         StartDate = ExecutionServer.DateMaxValue;
