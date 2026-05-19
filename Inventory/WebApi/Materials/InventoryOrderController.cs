ea02a2c2dae7c73866387f07f0127a261c318689
efrain-jerv <efrain.rodriguez@ontica.org>
2026-05-19 12:24:34 -0700

    Rfx Unnecesary web services removed

--- Inventory/WebApi/Materials/InventoryOrderController.cs
+++ Inventory/WebApi/Materials/InventoryOrderController.cs
@@ -19,111 +19,7 @@ namespace Empiria.Inventory.WebApi {
     #region Web Apis
 
     [HttpPost]
-    [Route("v8/order-management/inventory-orders/search")] 
-    public SingleObjectModel SearchInventoryOrderList([FromBody] InventoryOrderQuery query) {
-
-      using (var usecases = InventoryOrderUseCases.UseCaseInteractor()) {
-
-        InventoryOrderDataDto inventoryOrderDto = usecases.SearchInventoryOrder(query);
-
-        return new SingleObjectModel(this.Request, inventoryOrderDto);
-      }
-    }
-
-
-    [HttpGet]
-    [Route("v8/order-management/inventory-orders/{orderUID:guid}")]
-    public SingleObjectModel GetInventoryOrder([FromUri] string orderUID) {
-
-      using (var usecases = InventoryOrderUseCases.UseCaseInteractor()) {
-
-        InventoryHolderDto inventoryOrder = usecases.GetInventoryOrder(orderUID);
-
-        return new SingleObjectModel(this.Request, inventoryOrder);
-      }
-    }
-
-
-    [HttpGet]
-    [Route("v8/order-management/inventory-types")]
-    public CollectionModel GetInventoryOrderTypes() {
-
-      using (var usecases = InventoryOrderUseCases.UseCaseInteractor()) {
-
-        FixedList<NamedEntityDto> orderTypes = usecases.GetOrderTypes();
-
-        return new CollectionModel(this.Request, orderTypes);
-      }
-    }
-
-
-    [HttpGet]
-    [Route("v8/order-management/warehouses")]
-    public CollectionModel GetWarehouses() {
-
-      using (var usecases = InventoryOrderUseCases.UseCaseInteractor()) {
-
-        FixedList<NamedEntityDto> warehouses = usecases.GetWarehouses();
-
-        return new CollectionModel(this.Request, warehouses);
-      }
-    }
-
-
-    [HttpGet]
-    [Route("v8/order-management/inventory-orders/inventory-supervisor")]
-    public CollectionModel GetResponsibles() {
-
-      using (var usecases = InventoryOrderUseCases.UseCaseInteractor()) {
-
-        FixedList<NamedEntityDto> parties = usecases.GetPartiesByRol("Inventory-manager");
-
-        return new CollectionModel(this.Request, parties);
-      }
-    }
-
-
-    [HttpGet]
-    [Route("v8/order-management/inventory-orders/warehousemen")]
-    public CollectionModel GetWarehouseman() {
-
-      using (var usecases = InventoryOrderUseCases.UseCaseInteractor()) {
-
-        FixedList<NamedEntityDto> parties = usecases.GetPartiesByRol("Warehouseman");
-
-        return new CollectionModel(this.Request, parties);
-      }
-    }
-
-
-    [HttpPost]
-    [Route("v8/order-management/inventory-orders")]
-    public SingleObjectModel CreateInventoryOrder([FromBody] InventoryOrderFields fields) {
-
-      using (var usecases = InventoryOrderUseCases.UseCaseInteractor()) {
-
-        InventoryHolderDto inventoryOrder = usecases.CreateInventoryOrder(fields.WarehouseUID, fields);
-
-        return new SingleObjectModel(this.Request, inventoryOrder);
-      }
-    }
-
-
-    [HttpPost]
-    [Route("v8/order-management/inventory-orders/{orderUID}/items")]
-    public SingleObjectModel CreateInventoryOrderItem([FromUri] string orderUID, [FromBody] InventoryOrderItemFields fields) {
-
-      using (var usecases = InventoryOrderUseCases.UseCaseInteractor()) {
-
-        InventoryHolderDto inventoryOrder = usecases.CreateInventoryOrderItem(orderUID, fields);
-
-        return new SingleObjectModel(this.Request, inventoryOrder);
-      }
-    }
-
-
-    [HttpPost]
-    [Route("v8/order-management/inventory-orders/{orderUID:guid}/close")]
+    [Route("v8/order-management/inventory-orders/{orderUID:guid}/close_")]
     public SingleObjectModel CloseInventoryOrder([FromUri] string orderUID) {
 
       using (var usecases = InventoryOrderUseCases.UseCaseInteractor()) {
@@ -135,50 +31,8 @@ namespace Empiria.Inventory.WebApi {
     }
 
 
-    [HttpDelete]
-    [Route("v8/order-management/inventory-orders/{orderUID:guid}")]
-    public NoDataModel DeleteInventoryOrder([FromUri] string orderUID) {
-
-      using (var usecases = InventoryOrderUseCases.UseCaseInteractor()) {
-
-        usecases.DeleteInventoryOrder(orderUID);
-
-        return new NoDataModel(this.Request);
-      }
-    }
-
-
-    [HttpDelete]
-    [Route("v8/order-management/inventory-orders/{orderUID:guid}/items/{orderItemUID:guid}")]
-    public SingleObjectModel DeleteInventoryOrderItem([FromUri] string orderUID,
-                                                [FromUri] string orderItemUID) {
-
-      using (var usecases = InventoryOrderUseCases.UseCaseInteractor()) {
-
-        InventoryHolderDto inventoryOrder = usecases.DeleteInventoryOrderItem(orderUID, orderItemUID);
-
-        return new SingleObjectModel(this.Request, inventoryOrder);
-      }
-    }
-
-
     [HttpPut]
-    [Route("v8/order-management/inventory-orders/{orderUID:guid}")]
-
-    public SingleObjectModel UpdateInventoryOrder([FromUri] string orderUID,
-                                                  [FromBody] InventoryOrderFields fields) {
-
-      using (var usecases = InventoryOrderUseCases.UseCaseInteractor()) {
-
-        InventoryHolderDto inventoryOrder = usecases.UpdateInventoryOrder(orderUID, fields);
-
-        return new SingleObjectModel(this.Request, inventoryOrder);
-      }
-    }
-
-
-    [HttpPut]
-    [Route("v8/order-management/inventory-orders/{orderUID:guid}/items/{orderItemUID:guid}")]
+    [Route("v8/order-management/inventory-orders/{orderUID:guid}/items/{orderItemUID:guid}_")]
 
     public SingleObjectModel UpdateInventoryOrderItemQuantity([FromUri] string orderUID, [FromUri] string orderItemUID,
                                                               [FromBody] InventoryOrderItemFields fields) {
